49e2b034f0ff22393b49fabe8461cdb107454113
Lukas <lukasd2000@gmx.de>
2025-05-27 16:49:21 +0200

    Stilistische Updates

--- main.py
+++ main.py
@@ -4,16 +4,13 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+def calculate_interest(years, interest_rate, saving_rate):
+    actual_saldo = saving_rate * 12
+    interest_pa = (interest_rate - 1) * 100
 
-
-def calculate_interest(years, interestRate, savingRate):
-    actualSaldo = savingRate * 12
-    interestPA  = (interestRate - 1) * 100
-
-
-    for year in range(1, int(years)+1):
-        actualSaldo = (actualSaldo + savingRate * 12) * interestRate
-        print(f"Jahr {year}: {savingRate} €/Monat gespart, {interestPA:.2f} % Zins, Gesamtsaldo: {actualSaldo:.2f}€")
+    for year in range(1, int(years) + 1):
+        actual_saldo = (actual_saldo + saving_rate * 12) * interest_rate
+        print(f"Jahr {year}: {saving_rate} €/Monat gespart, {interest_pa:.2f} % Zins, Gesamtsaldo: {actual_saldo:.2f}€")
 
 
 def print_hi(name):
@@ -23,14 +20,13 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    years = int(input("Wie viele Jahre soll der Zins berechnet werden? \n"))
-    interestRate = int(input("Wie hoch ist der Zins in Prozent? \n"))
-    savingRate = int(input("Wie viel Geld soll pro Monat in € gespart werden? \n"))
-
+    input_Years = int(input("Wie viele Jahre soll der Zins berechnet werden? \n"))
+    input_interestRate = int(input("Wie hoch ist der Zins in Prozent? \n"))
+    input_savingRate = int(input("Wie viel Geld soll pro Monat in € gespart werden? \n"))
 
     print_hi('Nutzer')
 
-    calculate_interest(years, 1+interestRate/100, savingRate)
+    calculate_interest(input_Years, 1 + input_interestRate / 100, input_savingRate)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 #76669.50€
